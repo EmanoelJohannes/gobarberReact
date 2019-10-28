21cc03c820e581d81e7c75d2038ed2b6bf319361
unknown <emanoeljohannes@hotmail.com>
2019-10-28 13:00:07 -0300

    Segundo commit

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+
+import {Form, Input} from '@rocketseat/unform'
+
+import { signUpRequest } from '../../store/modules/auth/actions'
+
+const schema = Yup.object().shape({
+  email: Yup.string().email().required('Insira um email válido'),
+  password: Yup.string().min(4, 'No minimo 4 caracteres').required('A senha é obrigatória'),
+  name: Yup.string().required('O nome é obrigatório')
+});
 
 export default function SignUp() {
+
+  const dispatch = useDispatch();
+
+  function handleSubmit({name, email, password}){
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
-    <div />
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo"/>
+        <Input name="email" type="email" placeholder="Seu e-mail"/>
+        <Input name="password" type="password" placeholder="Sua senha secreta"/>
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já estou cadastrado</Link>
+      </Form>
+    </>
   );
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
+import {store} from '../store';
+
 export default function RouteWrapper({
     component: Component,
     isPrivate,
-    ... rest
-}){
-    const signed = false;
+    ...rest
+}) {
+    const {signed} = store.getState().auth;
 
     if (!signed && isPrivate){
         return <Redirect to="/" /> 
@@ -17,7 +22,14 @@ export default function RouteWrapper({
         return <Redirect to="/dashboard" />
     }
 
-    return <Route { ...rest } component={Component} />;
+    const Layout = signed ? DefaultLayout : AuthLayout;
+
+
+    return <Route { ...rest } render={props => (
+        <Layout>
+            <Component {...props} />
+        </Layout>
+    )} />;
 
 }
 
